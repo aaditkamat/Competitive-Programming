36680674fbde5aa10850332b6cb0b99dd9ce73b8
Aadit Kamat <aadit.k12@gmail.com>
2020-12-07 22:32:07 +0800

    Update typing

--- Advent of Code/2020/Python/day1.py
+++ Advent of Code/2020/Python/day1.py
@@ -1,11 +1,11 @@
 import argparse
 import sys
-from typing import List, Tuple
+from typing import List, Tuple, Set, Union
 
 
 # Part 1
-def two_sum(lst: list, total: int) -> Tuple[int, int]:
-    container: set[int] = set()
+def two_sum(lst: List[int], total: int) -> Union[Tuple[int, int],  None] :
+    container: Set[int] = set()
     for num in lst:
         if total - num in container:
             return (num, total - num)
@@ -14,8 +14,8 @@ def two_sum(lst: list, total: int) -> Tuple[int, int]:
 
 
 # Part 2
-def three_sum(lst: List[int], total: int) -> Tuple[int, int, int]:
-    container: set[int] = set()
+def three_sum(lst: List[int], total: int) -> Union[Tuple[int, int, int], None]:
+    container: Set[int] = set()
     for num in lst:
         tup = two_sum(lst, total - num)
         if tup:
@@ -29,12 +29,17 @@ if __name__ == "__main__":
     parser.add_argument(
         "input file", metavar="FILE", help="name of input file (day1.in)"
     )
+    args: argparse.Namespace = parser.parse_args()
     file_name: str = sys.argv[1]
     with open(file_name) as f:
         lst: List[int] = list(map(int, f.read().split()))
-        num1, num2 = two_sum(lst, 2020)
-        print(f"Part 1: The product of {num1} and {num2} is: {num1 * num2}")
-        num1, num2, num3 = three_sum(lst, 2020)
-        print(
-            f"Part 2: The product of {num1}, {num2} and {num3} is: {num1 * num2 * num3}"
-        )
+        result1: Union[Tuple[int, int], None] = two_sum(lst, 2020)
+        if result1:
+            num1, num2 = result1
+            print(f"Part 1: The product of {num1} and {num2} is: {num1 * num2}")
+        result2: Union[Tuple[int, int, int], None] = three_sum(lst, 2020)
+        if result2:
+            num1, num2, num3 = result2
+            print(
+                f"Part 2: The product of {num1}, {num2} and {num3} is: {num1 * num2 * num3}"
+            )
