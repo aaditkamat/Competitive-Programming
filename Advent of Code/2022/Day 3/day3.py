983f69ad66367d2d7f894a84cf9200179a82ed06
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2022-12-12 13:58:26 +0000

    Add Day 3 AOC 2022 solution

---
+++ Advent of Code/2022/Day 3/day3.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import string
+
+
+def parse_part1(file):
+    first_compartment, second_compartment = [], []
+    for line in file:
+        first_compartment.append(line[: len(line) // 2])
+        second_compartment.append(line[len(line) // 2:])
+    return pd.DataFrame({'first_compartment': first_compartment, 'second_compartment': second_compartment})
+
+
+def parse_part2(file):
+    lines = []
+    for line in file:
+        lines.append(line.strip('\n'))
+    return pd.DataFrame({'first_rucksack': lines[0:: 3], 'second_rucksack': lines[1:: 3], 'third_rucksack': lines[2:: 3]})
+
+
+def part_1():
+    path = '/kaggle/input/advent-of-code-2022/day3.txt'
+
+    with open(path) as file:
+        df = parse_part1(file)
+        item_types = []
+        first_compartment, second_compartment = df['first_compartment'], df['second_compartment']
+        for first_item, second_item in zip(first_compartment, second_compartment):
+            item_types.append(
+                set(first_item).intersection(set(second_item)).pop())
+        df['item_type'] = pd.Series(item_types)
+        df['priority'] = [string.ascii_letters.find(item) + 1 for item in df['item_type']]
+        print(f'Sum of the priorities of the item types is: {df["priority"].sum()}')
+
+
+def part_2():
+    path = '/kaggle/input/advent-of-code-2022/day3.txt'
+
+    with open(path) as file:
+        df = parse_part2(file)
+        badges = []
+        first_rucksack, second_rucksack, third_rucksack = df[
+            'first_rucksack'], df['second_rucksack'], df['third_rucksack']
+        for first_item, second_item, third_item in zip(first_rucksack, second_rucksack, third_rucksack):
+            badges.append(set(first_item).intersection(
+                set(second_item)).intersection(set(third_item)).pop())
+        df['badges'] = pd.Series(badges)
+        df['priority'] = [string.ascii_letters.find(badge) + 1 for badge in df['badges']]
+        print(f'Sum of the priorities of the item types is: {df["priority"].sum()}')
+
+
+if __name__ == '__main__':
+    part_1()
+    part_2()
