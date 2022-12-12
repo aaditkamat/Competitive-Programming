f64d622bdcc29d31c496f8bb699e66833ca8af70
Sourcery AI <>
2022-12-12 14:01:20 +0000

    'Refactored by Sourcery'

--- Advent of Code/2022/Day 3/day3.py
+++ Advent of Code/2022/Day 3/day3.py
@@ -11,10 +11,14 @@ def parse_part1(file):
 
 
 def parse_part2(file):
-    lines = []
-    for line in file:
-        lines.append(line.strip('\n'))
-    return pd.DataFrame({'first_rucksack': lines[0:: 3], 'second_rucksack': lines[1:: 3], 'third_rucksack': lines[2:: 3]})
+    lines = [line.strip('\n') for line in file]
+    return pd.DataFrame(
+        {
+            'first_rucksack': lines[::3],
+            'second_rucksack': lines[1::3],
+            'third_rucksack': lines[2::3],
+        }
+    )
 
 
 def part_1():
@@ -22,11 +26,13 @@ def part_1():
 
     with open(path) as file:
         df = parse_part1(file)
-        item_types = []
         first_compartment, second_compartment = df['first_compartment'], df['second_compartment']
-        for first_item, second_item in zip(first_compartment, second_compartment):
-            item_types.append(
-                set(first_item).intersection(set(second_item)).pop())
+        item_types = [
+            set(first_item).intersection(set(second_item)).pop()
+            for first_item, second_item in zip(
+                first_compartment, second_compartment
+            )
+        ]
         df['item_type'] = pd.Series(item_types)
         df['priority'] = [string.ascii_letters.find(item) + 1 for item in df['item_type']]
         print(f'Sum of the priorities of the item types is: {df["priority"].sum()}')
@@ -37,12 +43,17 @@ def part_2():
 
     with open(path) as file:
         df = parse_part2(file)
-        badges = []
         first_rucksack, second_rucksack, third_rucksack = df[
             'first_rucksack'], df['second_rucksack'], df['third_rucksack']
-        for first_item, second_item, third_item in zip(first_rucksack, second_rucksack, third_rucksack):
-            badges.append(set(first_item).intersection(
-                set(second_item)).intersection(set(third_item)).pop())
+        badges = [
+            set(first_item)
+            .intersection(set(second_item))
+            .intersection(set(third_item))
+            .pop()
+            for first_item, second_item, third_item in zip(
+                first_rucksack, second_rucksack, third_rucksack
+            )
+        ]
         df['badges'] = pd.Series(badges)
         df['priority'] = [string.ascii_letters.find(badge) + 1 for badge in df['badges']]
         print(f'Sum of the priorities of the item types is: {df["priority"].sum()}')
